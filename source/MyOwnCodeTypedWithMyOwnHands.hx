9c7582e2f84b2bd1ee63c310feb84df5163f0f4d
Emerald <87787233+TheEmeraldBlade1@users.noreply.github.com>
2023-11-29 15:14:01 -0800

    Update MyOwnCodeTypedWithMyOwnHands.hx

--- source/MyOwnCodeTypedWithMyOwnHands.hx
+++ source/MyOwnCodeTypedWithMyOwnHands.hx
@@ -1,9 +1,14 @@
 class MyOwnCodeTypedWithMyOwnHands
 {
-  public static var lmfao:Float = 0;
-
-  public static function ratingUpdate(gay:Int, lostcode:Bool, eatass:Float)
+  public static var lmfao:Float = 0.00;
+  public static var bisexual:Int = 0;
+  public static var pansexual:Float = 0.00;
+  public static var lesbian:Int = 0;
+  public static function ratingUpdate(gay:Int, gay2:Int, gay3:Float, gay4:Float)
   {
-    
+    lmfao += gay3;
+    pansexual += gay4;
+    bisexual += gay;
+    lesbian += gay2 * rng; // will add rng code when i get home
   }
 }
