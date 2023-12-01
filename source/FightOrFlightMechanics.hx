f6ffe656af6ef95aac1530d549345434e8cc56f5
Emerald <87787233+TheEmeraldBlade1@users.noreply.github.com>
2023-12-01 09:53:21 -0800

    Update FightOrFlightMechanics.hx

--- source/FightOrFlightMechanics.hx
+++ source/FightOrFlightMechanics.hx
@@ -1,3 +1,7 @@
-class FightOrFlightMechanices
+class FightOrFlightMechanics // ill just edit this and finish it when im home
 {
+	public static var femboys:Int = 0;
+	var gameSong:PlayState.SONG;
+	public static var cantaloupe:Int = 0;
+	var songNotes:Note;
 }
